b0ae43c7ed895d26d12873a5064e7fa26f3e28e5
RamRijal <rijram004@gmail.com>
2023-12-15 19:30:03 +0545

    Initial commit to react-website-1

---
+++ src/components/navbar.js
@@ -0,0 +1,58 @@
+import React, { useState } from 'react'
+import Logo from '../assets/Logo.svg' //Downloaded image
+import { BsCart2 } from 'react-icons/bs'
+import { HiOutlineBars3 } from 'react-icons/hi2' //Imported the react icons in terminal "npm install react-icons --save"
+import { Box, Drawer, ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material' //Imported material ui from mui.com
+import  HomeIcon  from '@mui/icons-material/Home' //using different built-in components from MaterialUI
+import  InfoIcon  from '@mui/icons-material/Info'
+import CommentRoundedIcon from '@mui/icons-material/CommentRounded'
+import PhoneRoundedIcon from '@mui/icons-material/PhoneRounded'
+import ShoppingCartRoundedIcon from '@mui/icons-material/ShoppingCartRounded'
+
+
+
+const Navbar = () => { //Navbar function and using usestate for menu
+    const [openMenu, setOpenMenu] = useState(false)
+    const menuOptions = [{
+        text: "Home",
+        icon: <HomeIcon />,
+    },
+    {
+        text: "About",
+        icon: <InfoIcon />,
+    },
+    {
+        text: "Testimonials",
+        icon: <CommentRoundedIcon />,
+    },
+    {
+        text: "Contact",
+        icon: <PhoneRoundedIcon />,
+    },
+    {
+        text: "Cart",
+        icon: <ShoppingCartRoundedIcon />,
+    }
+    ]
+    return (
+        <nav>
+            <div className='nav-logo-container'>
+                <img src={Logo} alt="" />
+            </div>
+            <div className='navbar-links-container'>
+                <a href=''>Home</a>
+                <a href=''>About</a>
+                <a href=''>Testimonials</a>
+                <a href=''>Contact</a>
+                <a href=''><BsCart2 className='navbar-cart-icon' /></a>
+                <button className='primary-button'>Booking Now</button>
+            </div>
+            <div className='navbar-menu-container'>
+                <HiOutlineBars3 onClick={() => setOpenMenu(true)} />
+            </div>
+
+        </nav>
+    )
+}
+
+export default Navbar
