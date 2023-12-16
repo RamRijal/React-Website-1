edfcd3f7e6072d9e92dfd2f2326f8ceacbb660a0
RamRijal <rijram004@gmail.com>
2023-12-16 21:50:39 +0545

    second commit to react-website-1

--- src/components/home.js
+++ src/components/home.js
@@ -1,10 +1,36 @@
 import React from 'react'
 import Navbar from './navbar.js'
+import BannerBg from '../assets/home-banner-bg.png' //Background image 
+import BannerImage from '../assets/home-banner-image.png' //Image used above background as content
+import { FiArrowRight } from 'react-icons/fi' //React icons library already installed
 
 const Home = () => {
   return (
     <div>
-      <Navbar/>
+      <div className='home-container'>
+        <Navbar />
+        <div className='home-banner-container'>
+          <div className='home-bannerImage-container'>
+            <img src={BannerBg} alt="" />
+          </div>
+
+          <div className='home-text-section'>
+            <h1 className='primary-heading'>
+              Welcome to the world of
+              <span style={{ color: '#FE9E0D' }}> Innovaton </span
+              >and
+              <span style={{ color: '#FE9E0D' }}> Coding! </span>
+            </h1>
+            <p className='primary-text'>
+              Discover the power of technology and its ability to transform ideas into reality. Explore my website for insightful articles, resources, and exciting projects.
+            </p>
+            <button className='secondary-button'>
+              Explore Now<FiArrowRight />
+            </button>
+          </div>
+        </div>
+      </div>
+
     </div>
   )
 }
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,9 +2,9 @@ import React, { useState } from 'react'
 import Logo from '../assets/Logo.svg' //Downloaded image
 import { BsCart2 } from 'react-icons/bs'
 import { HiOutlineBars3 } from 'react-icons/hi2' //Imported the react icons in terminal "npm install react-icons --save"
-import { Box, Drawer, ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material' //Imported material ui from mui.com
-import  HomeIcon  from '@mui/icons-material/Home' //using different built-in components from MaterialUI
-import  InfoIcon  from '@mui/icons-material/Info'
+import { Box, Drawer, List, ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material' //Imported material ui from mui.com
+import HomeIcon from '@mui/icons-material/Home' //using different built-in components from MaterialUI
+import InfoIcon from '@mui/icons-material/Info'
 import CommentRoundedIcon from '@mui/icons-material/CommentRounded'
 import PhoneRoundedIcon from '@mui/icons-material/PhoneRounded'
 import ShoppingCartRoundedIcon from '@mui/icons-material/ShoppingCartRounded'
@@ -12,8 +12,8 @@ import ShoppingCartRoundedIcon from '@mui/icons-material/ShoppingCartRounded'
 
 
 const Navbar = () => { //Navbar function and using usestate for menu
-    const [openMenu, setOpenMenu] = useState(false)
-    const menuOptions = [{
+    const [openMenu, setOpenMenu] = useState(false) //openMenu variable is set
+    const menuOptions = [{ //Navbar menu options with icon
         text: "Home",
         icon: <HomeIcon />,
     },
@@ -36,21 +36,47 @@ const Navbar = () => { //Navbar function and using usestate for menu
     ]
     return (
         <nav>
-            <div className='nav-logo-container'>
-                <img src={Logo} alt="" />
+
+            <div className='nav-logo-container' >
+                <a href='#'>
+                    <img src={Logo} alt="" />
+                </a>
             </div>
             <div className='navbar-links-container'>
                 <a href=''>Home</a>
                 <a href=''>About</a>
                 <a href=''>Testimonials</a>
-                <a href=''>Contact</a>
+                <a href='' className='contact'>Contact</a>
                 <a href=''><BsCart2 className='navbar-cart-icon' /></a>
                 <button className='primary-button'>Booking Now</button>
             </div>
             <div className='navbar-menu-container'>
                 <HiOutlineBars3 onClick={() => setOpenMenu(true)} />
-            </div>
 
+                {/* Here when icon is clicked useState sets openMenu true */}
+
+            </div>
+            <Drawer open={openMenu} onClose={() => setOpenMenu(false)} anchor='right'>
+               
+                {/* expands a box for menu and closes the menu again */}
+                
+                <Box sx={{ width: 250 }}
+                    role="presentation"
+                    onClick={() => setOpenMenu(false)}
+                    onKeyDown={() => setOpenMenu(false)}
+                >
+                    <List>
+                        {menuOptions.map((item) => (
+                            <ListItem key={item.text} disablePadding>
+                                <ListItemButton>
+                                    <ListItemIcon>{item.icon}</ListItemIcon>
+                                    <ListItemText primary={item.text} />
+                                </ListItemButton>
+                            </ListItem>
+                        ))}
+                    </List>
+                </Box>
+            </Drawer>
         </nav>
     )
 }
